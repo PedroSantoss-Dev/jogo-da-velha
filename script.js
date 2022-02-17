ee8d42f9ec9dca547e28a3e8c373c6730e4a8b59
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2022-02-16 22:14:20 -0300

    add troca de marca

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.querySelector('[data-board]');
 
 let isCircle;
+
+const placeMark =  (cell, classToAdd) => {
+    cell.classList.add(classToAdd);
+}
+const swapTurns = () => {
+    isCircle = !isCircle;
+    board.classList.remove('circle');
+    board.classList.remove('x');
+
+    if (isCircle) {
+        board.classList.add('circle');
+    }else{
+        board.classList.add('x');
+    }
+}
 
 const handleClick = (e) =>{
     //colocar marca  (x ou circle)
     const cell = e.target;
-    const classToAdd = isCircle ? 'cicle':'x';
-    cell.classList.add(classToAdd)
+    const classToAdd = isCircle ? 'circle':'x';
+    
+
+    placeMark(cell, classToAdd)
+    //mudar marca
+    swapTurns();
 } 
 
 for(const cell of cellElements) {
